6369d25e18196b15a0d38e7e13a9f748ea67a5ed
Tywin Cary <tywincary@gmail.com>
2023-10-01 18:23:20 -0700

    finished cleanser

--- datacleansing/datacleanser.py
+++ datacleansing/datacleanser.py
@@ -6,7 +6,9 @@ foldernames = ["name_basics/", "title_akas/", "title_basics/",
                 "title_crew/", "title_principals/", "title_ratings/"]
 
 # Trim name_basics by removing birthYear and deathYear
-nBasicsDF = pd.read_csv(filepath+foldernames[0]+filename, sep="\t")
+print("Reading in name_basics")
+nBasicsDF = pd.read_csv(filepath+foldernames[0]+filename, sep="\t", low_memory=False)
+nBasicsDF.set_index('nconst')
 
 # Dropping Columns
 nBasicsDF = nBasicsDF.drop('birthYear', axis=1)
@@ -14,7 +16,9 @@ nBasicsDF = nBasicsDF.drop('deathYear', axis=1)
 
 
 # Trim akas by region US and remove ordering, attributes, region, isOriginalTitle, and types
-akasDF = pd.read_csv(filepath+foldernames[1]+filename, sep="\t")
+print("Reading in akas")
+akasDF = pd.read_csv(filepath+foldernames[1]+filename, sep="\t", low_memory=False)
+akasDF.set_index('titleId')
 
 akasDF = akasDF[akasDF['region'] == 'US'] # Check for region US
 
@@ -25,30 +29,48 @@ akasDF = akasDF.drop('region', axis=1)
 akasDF = akasDF.drop('isOriginalTitle', axis=1)
 akasDF = akasDF.drop('types', axis=1)
 
-# Trim title_basics by movie and remove endYear
-tBasicsDF = pd.read_csv(filepath+foldernames[2]+filename, sep="\t")
+# Trim title_basics by movie and remove endYear nad titleType
+print("Reading in title_basics")
+tBasicsDF = pd.read_csv(filepath+foldernames[2]+filename, sep="\t", low_memory=False)
+tBasicsDF.set_index('tconst')
 
 tBasicsDF = tBasicsDF[tBasicsDF['titleType'] == 'movie'] # Check for movies
 
-tBasicsDF = tBasicsDF.drop('endYear', axis=1) # Dropping endYear column
+# Dropping Columns
+tBasicsDF = tBasicsDF.drop('endYear', axis=1)
+tBasicsDF = tBasicsDF.drop('titleType', axis=1)
 
-# Trim crew by movie and remove endYear
-crewDF = pd.read_csv(filepath+foldernames[3]+filename, sep="\t")
-
-crewDF = crewDF[crewDF['titleType'] == 'movie'] # Check for movies
-
-crewDF = crewDF.drop('endYear', axis=1) # Dropping endYear column
+# Read in crew
+print("Reading in crew")
+crewDF = pd.read_csv(filepath+foldernames[3]+filename, sep="\t", low_memory=False)
+crewDF.set_index('tconst')
 
 # Trim principals by removing ordering, category, job, and characters
-principalsDF = pd.read_csv(filepath+foldernames[4]+filename, sep="\t")
+print("Reading in principals")
+principalsDF = pd.read_csv(filepath+foldernames[4]+filename, sep="\t", low_memory=False)
 
 # Dropping Columns
 principalsDF = principalsDF.drop('ordering', axis=1)
-principalsDF = principalsDF.drop('category', axis=1)
 principalsDF = principalsDF.drop('job', axis=1)
 principalsDF = principalsDF.drop('characters', axis=1)
 
 # Trim ratings by removing numVotes
-ratingsDF = pd.read_csv(filepath+foldernames[5]+filename, sep="\t")
+print("Reading in ratings")
+ratingsDF = pd.read_csv(filepath+foldernames[5]+filename, sep="\t", low_memory=False)
+ratingsDF.set_index('tconst')
 
 ratingsDF = ratingsDF.drop('numVotes', axis=1)
+
+# Merge movie tables
+movieDF = pd.merge(tBasicsDF, akasDF, left_on='tconst', right_on='titleId')
+movieDF = movieDF.drop('titleId', axis=1)
+movieDF = pd.merge(movieDF,crewDF)
+movieDF = movieDF.drop_duplicates(subset='tconst')
+print(movieDF)
+movieDF.to_csv("movie_data.tsv", index=False, sep="\t")
+
+principalsDF.to_csv("principals_data.tsv", index=False, sep="\t")
+print(principalsDF.shape)
+
+nBasicsDF.to_csv("name_data.tsv", index=False, sep="\t")
+print(nBasicsDF.shape)
