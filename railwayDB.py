9a5c352c1e3cd4ce2cb3372014c9944e451ff195
Everett Cooley <39889137+EverettCooley@users.noreply.github.com>
2023-11-15 19:35:26 -0700

    Update railwayDB.py

--- railwayDB.py
+++ railwayDB.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_HOST = os.getenv('MONGO_HOST')
-MONGO_PORT = os.getenv('MONGO_PORT')
+MONGO_PORT = int(os.getenv('MONGO_PORT'))
 MONGO_USER = os.getenv('MONGO_USER')
 MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
